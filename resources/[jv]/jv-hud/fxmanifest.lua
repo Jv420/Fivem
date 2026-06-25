6474f13a40e7f2f1f8f13bb41716779dbe3c784d
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-06-25 18:22:55 +0200

    Add Jv420 HUD manifest

---
+++ resources/[jv]/jv-hud/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Jv420'
+description 'Jv420 simple starter HUD'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
