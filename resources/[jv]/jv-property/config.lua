a7479a1813d6118fc221fa6fc9cf7c920982f6bf
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-06-25 18:37:16 +0200

    Add property config

---
+++ resources/[jv]/jv-property/config.lua
@@ -0,0 +1,8 @@
+Config = {}
+
+Config.Properties = {
+    { id = 'apartment_1', label = 'Starter Appartement', price = 25000, rent = 500 },
+    { id = 'house_vespucci', label = 'Vespucci Huis', price = 125000, rent = 0 },
+    { id = 'house_sandy', label = 'Sandy Shores Huis', price = 85000, rent = 0 },
+    { id = 'villa_vinewood', label = 'Vinewood Villa', price = 750000, rent = 0 }
+}
