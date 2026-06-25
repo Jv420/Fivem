f4f30304e0a354bf965a04bec131a1ad91cdb157
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-06-25 18:42:37 +0200

    Add company config starter

---
+++ resources/[jv]/jv-company/config.lua
@@ -0,0 +1,9 @@
+Config = {}
+
+Config.Companies = {
+    { id = 'food_1', label = 'BurgerShot', price = 250000, payout = 1200 },
+    { id = 'food_2', label = 'UwU Cafe', price = 220000, payout = 1000 },
+    { id = 'garage_1', label = 'Mechanic Shop', price = 400000, payout = 1800 },
+    { id = 'taxi_1', label = 'Taxi Office', price = 175000, payout = 900 },
+    { id = 'cargo_1', label = 'Cargo Office', price = 350000, payout = 1600 }
+}
