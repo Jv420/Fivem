ebf31397351765798cc7da993c3379361a3ff1b3
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-06-25 18:26:02 +0200

    Add weapon shop manifest

---
+++ resources/[jv]/jv-weaponshop/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Jv420'
+description 'Jv420 starter legal weapon shop for QBCore'
+version '1.0.0'
+
+shared_script 'config.lua'
+server_script 'server.lua'
