2a1490238ee71dd6a3286d97abd4c37667beb56b
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-06-25 13:20:26 +0200

    Add Discord logs manifest

---
+++ resources/[jv]/jv-discordlogs/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Jv420'
+description 'Simple Discord logging resource for FiveM'
+version '1.0.0'
+
+server_scripts {
+    'config.lua',
+    'server.lua'
+}
