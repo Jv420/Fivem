b918a2e8fae86890ec92d5ead5760150f8efa8ea
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-06-25 13:20:46 +0200

    Add Discord logs config

---
+++ resources/[jv]/jv-discordlogs/config.lua
@@ -0,0 +1,8 @@
+Config = {}
+
+-- Vul hier je eigen Discord webhook in.
+-- Laat leeg als je logs tijdelijk wilt uitschakelen.
+Config.Webhook = ''
+
+Config.ServerName = 'Jv420 FiveM'
+Config.Color = 3447003
