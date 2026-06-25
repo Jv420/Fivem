ee2cc2139d306d58e7dc1d83196e5e5da65dbfac
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-06-25 22:26:30 +0200

    Add delfzijl_anwb manifest

---
+++ delfzijl-rp/resources/[delfzijl]/delfzijl_anwb/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+author 'Delfzijl RP'
+description 'delfzijl_anwb - Delfzijl RP starter resource'
+version '1.0.0'
+shared_scripts {'@ox_lib/init.lua','shared/config.lua'}
+client_scripts {'client/main.lua'}
+server_scripts {'@oxmysql/lib/MySQL.lua','server/main.lua'}
