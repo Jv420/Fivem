29f996d705d623f89b3761df115f1410bea9981b
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-06-25 18:24:31 +0200

    Add starter jobs manifest

---
+++ resources/[jv]/jv-starterjobs/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Jv420'
+description 'Jv420 starter jobs for QBCore'
+version '1.0.0'
+
+shared_script 'config.lua'
+client_script 'client.lua'
+server_script 'server.lua'
