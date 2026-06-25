218673f46de621c239b76f2e4a19e0ce4bd0f7cf
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-06-25 13:18:20 +0200

    Add loading screen manifest

---
+++ resources/[jv]/jv-loading/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Jv420'
+description 'Simple Jv420 FiveM loading screen'
+version '1.0.0'
+
+loadscreen 'html/index.html'
+loadscreen_cursor 'yes'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
