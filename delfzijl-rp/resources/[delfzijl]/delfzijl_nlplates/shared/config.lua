9506ed1559552422008f4196789c2b76c2477981
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-06-27 15:55:24 +0200

    Expand nlplates config

--- delfzijl-rp/resources/[delfzijl]/delfzijl_nlplates/shared/config.lua
+++ delfzijl-rp/resources/[delfzijl]/delfzijl_nlplates/shared/config.lua
@@ -1,4 +1,25 @@
 Config = {}
+
 Config.Locale = 'nl'
 Config.ServerName = 'Delfzijl RP'
 Config.CityName = 'Delfzijl'
+
+-- Nederlandse GTA/FiveM kenteken formats.
+-- GTA kentekenplaten zijn maximaal 8 tekens zichtbaar.
+Config.Prefixes = {
+    'DZ', -- Delfzijl
+    'GR', -- Groningen
+    'NL',
+    'RDW'
+}
+
+Config.Formats = {
+    'XX999X',
+    '99XXX9',
+    'X999XX',
+    'XXX999'
+}
+
+Config.ApplyToCurrentVehicleCommand = 'setnlplate'
+Config.GenerateCommand = 'genplate'
+Config.Debug = false
