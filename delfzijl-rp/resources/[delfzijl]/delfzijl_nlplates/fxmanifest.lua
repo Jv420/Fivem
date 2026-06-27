bbb06e8a684e97857aedbcf0e521d8777361389d
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-06-27 15:55:53 +0200

    Update nlplates manifest simple

--- delfzijl-rp/resources/[delfzijl]/delfzijl_nlplates/fxmanifest.lua
+++ delfzijl-rp/resources/[delfzijl]/delfzijl_nlplates/fxmanifest.lua
@@ -1,9 +1,20 @@
 fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
+
 author 'Delfzijl RP'
-description 'delfzijl_nlplates - Delfzijl RP starter resource'
-version '1.0.0'
-shared_scripts {'@ox_lib/init.lua','shared/config.lua'}
-client_scripts {'client/main.lua'}
-server_scripts {'@oxmysql/lib/MySQL.lua','server/main.lua'}
+description 'Nederlandse kentekens voor Delfzijl RP'
+version '1.1.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'shared/config.lua'
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    'server/main.lua'
+}
