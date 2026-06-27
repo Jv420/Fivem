46a347a736f9bcb424216e4f37d8d6247ff1a738
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-06-27 15:54:12 +0200

    Fix ESX imports for illegal resource

--- delfzijl-rp/resources/[delfzijl]/delfzijl_illegal/fxmanifest.lua
+++ delfzijl-rp/resources/[delfzijl]/delfzijl_illegal/fxmanifest.lua
@@ -1,9 +1,30 @@
 fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
+
 author 'Delfzijl RP'
-description 'delfzijl_illegal - Delfzijl RP starter resource'
-version '1.0.0'
-shared_scripts {'@ox_lib/init.lua','shared/config.lua'}
-client_scripts {'client/main.lua'}
-server_scripts {'@oxmysql/lib/MySQL.lua','server/main.lua'}
+description 'delfzijl_illegal - RP routes voor Delfzijl RP'
+version '1.0.1'
+
+dependencies {
+    'ox_lib',
+    'oxmysql',
+    'ox_inventory',
+    'ox_target',
+    'es_extended'
+}
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    '@es_extended/imports.lua',
+    'shared/config.lua'
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
+}
