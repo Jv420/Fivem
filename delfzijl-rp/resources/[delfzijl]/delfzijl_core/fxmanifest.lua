d724e067a26f647dd69b746693b72048f92b5f26
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-06-27 15:59:14 +0200

    Update delfzijl core manifest

--- delfzijl-rp/resources/[delfzijl]/delfzijl_core/fxmanifest.lua
+++ delfzijl-rp/resources/[delfzijl]/delfzijl_core/fxmanifest.lua
@@ -1,9 +1,22 @@
 fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
+
 author 'Delfzijl RP'
-description 'delfzijl_core - Delfzijl RP starter resource'
-version '1.0.0'
-shared_scripts {'@ox_lib/init.lua','shared/config.lua'}
-client_scripts {'client/main.lua'}
-server_scripts {'@oxmysql/lib/MySQL.lua','server/main.lua'}
+description 'Core functies voor Delfzijl RP'
+version '1.1.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    '@es_extended/imports.lua',
+    'shared/config.lua'
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
+}
