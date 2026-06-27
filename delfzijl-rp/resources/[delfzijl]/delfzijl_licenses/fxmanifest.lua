ae763d5c07d0f0c2dce07595c9538a5536900992
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-06-27 15:54:30 +0200

    Fix ESX imports for licenses resource

--- delfzijl-rp/resources/[delfzijl]/delfzijl_licenses/fxmanifest.lua
+++ delfzijl-rp/resources/[delfzijl]/delfzijl_licenses/fxmanifest.lua
@@ -1,9 +1,30 @@
 fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
+
 author 'Delfzijl RP'
-description 'delfzijl_licenses - Delfzijl RP starter resource'
-version '1.0.0'
-shared_scripts {'@ox_lib/init.lua','shared/config.lua'}
-client_scripts {'client/main.lua'}
-server_scripts {'@oxmysql/lib/MySQL.lua','server/main.lua'}
+description 'delfzijl_licenses - licenties voor Delfzijl RP'
+version '1.0.1'
+
+dependencies {
+    'ox_lib',
+    'oxmysql',
+    'ox_target',
+    'es_extended',
+    'esx_license'
+}
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    '@es_extended/imports.lua',
+    'shared/config.lua'
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
+}
