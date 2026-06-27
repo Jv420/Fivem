1ad302b913c4007170916785dad7c7c774831646
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-06-27 16:01:40 +0200

    Fix garage manifest for ESX

--- delfzijl-rp/resources/[delfzijl]/delfzijl_garage/fxmanifest.lua
+++ delfzijl-rp/resources/[delfzijl]/delfzijl_garage/fxmanifest.lua
@@ -3,9 +3,17 @@ game 'gta5'
 lua54 'yes'
 
 author 'Delfzijl RP'
-description 'Garage module voor Delfzijl RP'
-version '1.0.0'
+description 'Werkende ESX garage voor Delfzijl RP'
+version '1.1.0'
 
-shared_script '@ox_lib/init.lua'
+shared_scripts {
+    '@ox_lib/init.lua',
+    '@es_extended/imports.lua'
+}
+
 client_script 'client.lua'
-server_script 'server.lua'
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server.lua'
+}
