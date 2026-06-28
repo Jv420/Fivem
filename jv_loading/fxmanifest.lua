ac7603e80f883a164fb4bb295ccdcb07e8eefaac
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-06-28 13:01:24 +0200

    Add premium Delfzijl loading screen resource

---
+++ jv_loading/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'jv_loading'
+author 'Jv420 / ChatGPT'
+description 'Premium Delfzijl loading screen voor FiveM'
+version '2.0.0'
+
+loadscreen 'html/index.html'
+loadscreen_cursor 'yes'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
